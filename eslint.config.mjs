678aefd251647410d3e5a83e7ae248d1f40e01ed
Vitor Fraga <vitorfragaps@gmail.com>
2025-05-26 21:44:28 -0300

    chore: add prettier and eslint setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,33 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+import { FlatCompat } from "@eslint/eslintrc";
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "no-unused-vars": "error",
+    },
+    languageOptions: {
+      parserOptions: {
+        sourceType: "module",
+        ecmaVersion: "latest",
+      },
+    },
+  },
 ];
 
 export default eslintConfig;
