bf521b52986d5b0d53174e6b807c6680e886a97a
Vitor Fraga <vitorfragaps@gmail.com>
2025-06-01 16:04:45 -0300

    feat: add StatsCards component to display key metrics on the dashboard

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,4 +1,5 @@
-import { eq } from "drizzle-orm";
+import dayjs from "dayjs";
+import { and, count, eq, gte, lte, sum } from "drizzle-orm";
 import { Metadata } from "next";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
@@ -13,16 +14,27 @@ import {
   PageTitle,
 } from "@/components/ui/page-container";
 import { db } from "@/db";
-import { usersToClinicsTable } from "@/db/schema";
+import {
+  appointmentsTable,
+  doctorsTable,
+  patientsTable,
+  usersToClinicsTable,
+} from "@/db/schema";
 import { auth } from "@/lib/auth";
 
+import { StatsCards } from "../appointments/_components/stats-card";
+// import { StatsCards } from "../appointments/_components/stats-card";
 import { DatePicker } from "./_components/date-picker";
 
 export const metadata: Metadata = {
   title: "Dashboard | Doutor Agenda",
 };
 
-const DashboardPage = async () => {
+interface DashboardPageProps {
+  searchParams: Promise<{ from: string; to: string }>;
+}
+
+const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
@@ -41,19 +53,82 @@ const DashboardPage = async () => {
     return redirect("/clinic-form");
   }
 
+  const { from, to } = await searchParams;
+  if (!from || !to) {
+    redirect(
+      `/dashboard?from=${dayjs().format("YYYY-MM-DD")}&to=${dayjs().add(1, "month").format("YYYY-MM-DD")}`,
+    );
+  }
+
+  console.log(`From: ${from}, To: ${to}`);
+
+  const [[totalRevenue], [totalAppointments], [totalPatients], [totalDoctors]] =
+    await Promise.all([
+      db
+        .select({
+          total: sum(appointmentsTable.appointmentPriceInCents),
+        })
+        .from(appointmentsTable)
+        .where(
+          and(
+            eq(appointmentsTable.clinicId, clinics[0].clinicId),
+            gte(appointmentsTable.date, new Date(from)),
+            lte(appointmentsTable.date, new Date(to)),
+          ),
+        ),
+      db
+        .select({
+          total: count(),
+        })
+        .from(appointmentsTable)
+        .where(
+          and(
+            eq(appointmentsTable.clinicId, clinics[0].clinicId),
+            gte(appointmentsTable.date, new Date(from)),
+            lte(appointmentsTable.date, new Date(to)),
+          ),
+        ),
+      db
+        .select({
+          total: count(),
+        })
+        .from(patientsTable)
+        .where(eq(patientsTable.clinicId, clinics[0].clinicId)),
+      db
+        .select({
+          total: count(),
+        })
+        .from(doctorsTable)
+        .where(eq(doctorsTable.clinicId, clinics[0].clinicId)),
+    ]);
+
+  console.log(
+    `Total Revenue: ${totalRevenue.total}, Total Appointments: ${totalAppointments.total}, Total Patients: ${totalPatients.total}, Total Doctors: ${totalDoctors.total}`,
+  );
+
   return (
     <PageContainer>
       <PageHeader>
         <PageHeaderContent>
-          <PageTitle>Médicos</PageTitle>
-          <PageDescription>Gerencie os médicos em sua clínica.</PageDescription>
+          <PageTitle>Dashboard</PageTitle>
+          <PageDescription>
+            Acesse uma visão geral detalhada das principais métricas e
+            resultados dos pacientes
+          </PageDescription>
         </PageHeaderContent>
 
         <PageActions>
           <DatePicker />
         </PageActions>
       </PageHeader>
-      <PageContent>conteudo</PageContent>
+      <PageContent>
+        <StatsCards
+          totalRevenue={Number(totalRevenue.total) || 0}
+          totalAppointments={totalAppointments.total || 0}
+          totalPatients={totalPatients.total || 0}
+          totalDoctors={totalDoctors.total || 0}
+        />
+      </PageContent>
     </PageContainer>
   );
 };
